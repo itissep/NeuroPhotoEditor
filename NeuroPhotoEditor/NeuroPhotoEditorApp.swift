367782814a5b68a060ffb4058e8075a6a2af0233
itissep <someonenewish@yandex.ru>
2025-02-02 10:21:21 +0300

    Initial Commit

---
+++ NeuroPhotoEditor/NeuroPhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuroPhotoEditorApp.swift
+//  NeuroPhotoEditor
+//
+//  Created by Уля on 02.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct NeuroPhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
